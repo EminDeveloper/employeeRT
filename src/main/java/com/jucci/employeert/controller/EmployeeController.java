f583270ffff63ae9540bfcb3f36a4350fb1f2734
Spring-stack <Spring-stack>
2021-12-14 09:58:04 -0800

    add header to getEmployees

--- src/main/java/com/jucci/employeert/controller/EmployeeController.java
+++ src/main/java/com/jucci/employeert/controller/EmployeeController.java
@@ -1,16 +1,15 @@
 package com.jucci.employeert.controller;
 
-
 import com.jucci.employeert.dao.EmployeeDAO;
 import com.jucci.employeert.domain.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class EmployeeController {
 
     @Autowired
@@ -19,30 +18,33 @@ public class EmployeeController {
     @RequestMapping("/")
     @ResponseBody
     public String welcome() {
-        return "Welcome to RestTemplate Example.";
+        return "Welcome to RestTemplate.";
     }
 
     // http://localhost:8080/employees
     // http://localhost:8080/employees.xml
     // http://localhost:8080/employees.json
-    @RequestMapping(value = "/employees",
-            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
-            method = RequestMethod.GET)
-    @ResponseBody
-    public List<Employee> getEmployees() {
+    @GetMapping(value = "/employees",
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+    )
+    public ResponseEntity<List<Employee>> getEmployees(
+            @RequestHeader(name = "X-COM-PERSIST", required = true) String headerPersist,
+            @RequestHeader(name = "X-COM-LOCATION", defaultValue = "ASIA") String headerLocation
+    ) {
         List<Employee> list = employeeDAO.getAllEmployees();
-        return list;
+        return ResponseEntity.ok(list);
     }
 
     // http://localhost:8080/employee/{empNo}
     // http://localhost:8080/employee/{empNo}.xml
     // http://localhost:8080/employee/{empNo}.json
     @RequestMapping(value = "/employee/{empNo}",
-            method = RequestMethod.GET, //
-            produces = { MediaType.APPLICATION_JSON_VALUE, //
-                    MediaType.APPLICATION_XML_VALUE })
+            method = RequestMethod.GET,
+            produces = {MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE})
     @ResponseBody
-    public Employee getEmployee(@PathVariable("empNo") String empNo) {
+    public Employee getEmployee(
+            @PathVariable("empNo") String empNo) {
         return employeeDAO.getEmployee(empNo);
     }
 
@@ -52,8 +54,8 @@ public class EmployeeController {
 
     @RequestMapping(value = "/employee",
             method = RequestMethod.POST,
-            produces = { MediaType.APPLICATION_JSON_VALUE,
-                    MediaType.APPLICATION_XML_VALUE })
+            produces = {MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE})
     @ResponseBody
     public Employee addEmployee(@RequestBody Employee emp) {
 
@@ -62,27 +64,27 @@ public class EmployeeController {
         return employeeDAO.addEmployee(emp);
     }
 
-    // URL:
     // http://localhost:8080/employee
     // http://localhost:8080/employee.xml
     // http://localhost:8080/employee.json
-    @RequestMapping(value = "/employee", //
-            method = RequestMethod.PUT, //
-            produces = { MediaType.APPLICATION_JSON_VALUE, //
-                    MediaType.APPLICATION_XML_VALUE })
+    @RequestMapping(value = "/employee",
+            method = RequestMethod.PUT,
+            produces = {MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE})
     @ResponseBody
-    public Employee updateEmployee(@RequestBody Employee emp) {
+    public Employee updateEmployee(
+            @RequestBody Employee emp
+    ) {
 
         System.out.println("(Service Side) Editing employee: " + emp.getEmpNo());
 
         return employeeDAO.updateEmployee(emp);
     }
 
-    // URL:
     // http://localhost:8080/employee/{empNo}
-    @RequestMapping(value = "/employee/{empNo}", //
-            method = RequestMethod.DELETE, //
-            produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+    @RequestMapping(value = "/employee/{empNo}",
+            method = RequestMethod.DELETE,
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     @ResponseBody
     public void deleteEmployee(@PathVariable("empNo") String empNo) {
 
